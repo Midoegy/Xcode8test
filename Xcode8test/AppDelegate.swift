396517ffa4848e5cee24a4fc08077c991c94b8d0
Mohamad Elian <mohamadelian@mohamads-mbp.lan>
2017-04-08 19:02:00 -0500

    Initial Commit

---
+++ Xcode8test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode8test
+//
+//  Created by Mohamad Elian on 4/8/17.
+//  Copyright © 2017 Mohamad Elian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode8test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8test
+//
+//  Created by Mohamad Elian on 4/8/17.
+//  Copyright © 2017 Mohamad Elian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
